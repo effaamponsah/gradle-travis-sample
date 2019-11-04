fd5ff649f554175fffc94940c3badf29cd9aa53b
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-11-04 09:53:41 +0000

    Actors created

---
+++ src/main/java/io/turntabl/Client.java
@@ -0,0 +1,7 @@
+package io.turntabl;
+
+public abstract class Client {
+    String name;
+    String ID;
+    ServiceLevel serviceLevel;
+}
---
+++ src/main/java/io/turntabl/PrivateClient.java
@@ -0,0 +1,8 @@
+package io.turntabl;
+
+public class PrivateClient extends Client implements ClientName {
+    @Override
+    public String getName() {
+        return null;
+    }
+}
